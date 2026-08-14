27362baadf354adc6d3f7daacdf00e13ca500f8e
Marc Pope <marcpope@me.com>
2026-08-14 09:23:18 -0400

    v2.90.0

--- agent/bbs-agent.py
+++ agent/bbs-agent.py
@@ -47,7 +47,7 @@ if not hasattr(subprocess, "run"):
     subprocess.run = _subprocess_run
     subprocess.CompletedProcess = _CompletedProcess
 
-AGENT_VERSION = "2.82.0"
+AGENT_VERSION = "2.90.0"
 BORG_PATH = None  # Resolved in get_system_info()
 IS_WINDOWS = sys.platform == "win32"
 
